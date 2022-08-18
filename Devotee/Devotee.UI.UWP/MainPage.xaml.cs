6f0f6d1648740495e2538458759e64ee6e3d9350
Sharp0802 <sharp0802.business@gmail.com>
2022-08-18 23:59:04 +0900

    Upload UWP proj

---
+++ Devotee/Devotee.UI.UWP/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Core;
+
+using muxc = Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Devotee.UI.UWP;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+        titleBar.ButtonBackgroundColor = Colors.Transparent;
+        titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+        var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+        coreTitleBar.ExtendViewIntoTitleBar = true;
+    }
+}
